f3891b4cf0e353515818d93aec2cbbbdd269c74d
Mi-lex <mishin-lex@mail.ru>
2020-02-09 11:53:18 +0300

    finish styling checkout page

---
+++ resources/js/components/ContactForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import StyledForm from './StyledForm';
+
+const ContactForm = () => {
+	return (
+		<StyledForm>
+			<input type="number" id="input-number" placeholder="Number" />
+			<textarea name="address" type="text" placeholder="Address" />
+			<input type="submit" value="Checkout" id="input-submit" />
+		</StyledForm>
+	);
+};
+
+export default ContactForm;
--- resources/js/pages/Checkout.jsx
+++ resources/js/pages/Checkout.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React  from 'react';
+import classes from '../../css/modules/Checkout.module.css';
+import Receipt from '../components/Receipt';
+import ContactForm from '../components/ContactForm';
 
 const Checkout = () => {
-	return <h1>Home</h1>;
+	return (
+		<div className="container lg d-flex justify-content-around flex-wrap">
+			<h2 className={classes.title}>Checkout</h2>
+			<ContactForm />
+			<Receipt cartItems={[]} />
+		</div>
+	);
 };
 
 export default Checkout;
