9034c7f67b11fd3a28c5f243dd5ae8f03d684a06
Mi-lex <mishin-lex@mail.ru>
2020-02-11 17:19:13 +0300

    fix undefined import

--- resources/js/pages/index.js
+++ resources/js/pages/index.js
@@ -3,4 +3,4 @@ export { default as Menu } from './Menu';
 export { default as Checkout } from './Checkout';
 export { default as Orders } from './Orders';
 export { default as Login } from './Login';
-export { default as Register } from './Register';
+export { default as SignUp } from './SignUp';
