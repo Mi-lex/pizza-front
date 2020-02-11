4bc9d8dcaf691a3505c88488dd03cd941972f8fe
Mi-lex <mishin-lex@mail.ru>
2020-02-11 16:52:18 +0300

    create actions and reducer for menu-items

---
+++ resources/js/redux/ducks/menu-items/actions.js
@@ -0,0 +1,31 @@
+const FETCH_ITEMS_REQUEST = 'FETCH_ITEMS';
+const FETCH_ITEMS_PENDING = 'FETCH_ITEMS';
+const FETCH_ITEMS_SUCCESS = 'FETCH_ITEMS_SUCCESS';
+const FETCH_ITEMS_ERROR = 'FETCH_ITEMS_ERROR';
+
+export const fetchItemsRequest = () => ({
+	type: FETCH_ITEMS_REQUEST,
+});
+
+export const fetchItemsSuccess = (items) => ({
+	type: FETCH_ITEMS_SUCCESS,
+	payload: items,
+});
+
+export const fetchItemsError = (error) => ({
+	type: FETCH_ITEMS_ERROR,
+	payload: error,
+});
+
+export default {
+	fetchItemsRequest,
+	fetchItemsError,
+	fetchItemsSuccess,
+};
+
+export const types = {
+	FETCH_ITEMS_REQUEST,
+	FETCH_ITEMS_PENDING,
+	FETCH_ITEMS_SUCCESS,
+	FETCH_ITEMS_ERROR,
+};
